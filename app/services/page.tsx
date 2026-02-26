1710a0a6a2db7136172f11e40f7487298c53f92c
Zack Forssberg <zack.forssberg@iths.se>
2026-02-26 17:15:05 +0100

    added services page

---
+++ app/services/page.tsx
@@ -0,0 +1,11 @@
+import TopControls from "@/components/TopControls";
+
+export default function Services() {
+    return (
+      <main className="relative">
+        <TopControls />
+        <div>
+        </div>
+      </main>
+    );
+  }
